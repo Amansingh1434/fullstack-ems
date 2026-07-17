7d0b75ecc7fda347f81c72f12775c3467ccf4456
Aman Singh <amansingh5001t@gmaill.com>
2026-07-17 12:40:31 +0530

    api created for Dashboard data

---
+++ server/routes/dashboardRoutes.js
@@ -0,0 +1,8 @@
+  import {Router} from "express"
+import { protect } from "../middleware/auth.js"
+import { getDashboard } from "../controllers/dashboardControllers.js"
+
+  const dashboardRouter=Router()
+dashboardRouter.get("/",protect,getDashboard)
+
+export default dashboardRouter;
