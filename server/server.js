cd1e9ae1bb6b06c3d37160baa8a3efb829fd0d9b
Aman Singh <amansingh5001t@gmaill.com>
2026-07-18 13:00:46 +0530

    backend completded

--- server/server.js
+++ server/server.js
@@ -10,7 +10,8 @@ import attendanceRouter from './routes/attendanceRoutes.js';
 import leaveRouter from './routes/leaveRoutes.js';
 import payslipRouter from './routes/payslipRoutes.js';
 import dashboardRouter from './routes/dashboardRoutes.js';
-
+import { serve } from "inngest/express";
+import { inngest, functions } from "./innggest/index.js"
 
  const app=express()
  const PORT = process.env.PORT || 4000;
@@ -29,6 +30,7 @@ app.use("/api/attendance",attendanceRouter)
 app.use("/api/leave",leaveRouter)
 app.use("/api/payslips",payslipRouter)
 app.use("/api/dashboard",dashboardRouter)
+app.use("/api/inngest", serve({ client: inngest, functions }));
 
 await connectDB()
 app.listen(PORT,()=>console.log(`server running on port ${PORT}`))
